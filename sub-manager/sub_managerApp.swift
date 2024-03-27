77725bd06f979192604a5c560c021606e4e03b2a
Daniel Chudy <dchudy@zuora.com>
2024-03-27 18:54:42 -0400

    Initial Commit

---
+++ sub-manager/sub_managerApp.swift
@@ -0,0 +1,32 @@
+//
+//  sub_managerApp.swift
+//  sub-manager
+//
+//  Created by Daniel Chudy on 3/27/24.
+//
+
+import SwiftUI
+import SwiftData
+
+@main
+struct sub_managerApp: App {
+    var sharedModelContainer: ModelContainer = {
+        let schema = Schema([
+            Item.self,
+        ])
+        let modelConfiguration = ModelConfiguration(schema: schema, isStoredInMemoryOnly: false)
+
+        do {
+            return try ModelContainer(for: schema, configurations: [modelConfiguration])
+        } catch {
+            fatalError("Could not create ModelContainer: \(error)")
+        }
+    }()
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+        .modelContainer(sharedModelContainer)
+    }
+}
